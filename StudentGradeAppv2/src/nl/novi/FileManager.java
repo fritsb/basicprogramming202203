af3cc0af71424d759527b7c28deda8a412b59bfa
Frits <frits@streeksoft.nl>
2022-06-22 15:59:28 +0200

    Added extra code for first 2 lines
    In comment also extra code for last few lines

--- StudentGradeAppv2/src/nl/novi/FileManager.java
+++ StudentGradeAppv2/src/nl/novi/FileManager.java
@@ -30,10 +30,16 @@ public class FileManager {
         File dataFile = new File("data.txt");
         try {
             Scanner dataScanner = new Scanner(dataFile);
+             int i = 0; // Extra variabele die wordt gebruikt als counter
+            // Er zou ook een ArrayList<String> aangemaakt kunnen worden die wordt gevuld met regels uit het tekst-bestand, bijv ArrayList<String> dataLines = new ArrayList<String>();
             while(dataScanner.hasNextLine()) {
-                String dataLine = dataScanner.nextLine();
-                System.out.println("Data: " + dataLine);
+                String dataLine = dataScanner.nextLine(); // Hier zou je dus de ArrayList<String> kunnen vullen met data via de add-methode, bijvoorbeeld dataLines.add(dataScanner.nextLine());
+                System.out.println("Data: " + dataLine); // dataLine kan je vervangen door dataLines.get(dataLines.size() - 1);;
+                i++;  // Hier wordt de counter met 1 verhoogd
+                if(i == 2) break; // Deze regel code zorgt ervoor dat er maximaal 2 regels worden gelezen. 
             }
+            // Bij gebruik van de ArrayList dataLines kan je de laatste paar regels eventueel opvragen, bijv met subList-methode
+            // Zie ook: https://stackoverflow.com/questions/14605999/getting-the-last-three-elements-from-a-list-arraylist
             dataScanner.close();
         }
         catch(Exception ex) {
